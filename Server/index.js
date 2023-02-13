217c98d5c605464e69fbf41ae829555dc6259fd7
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-02-13 21:54:23 +0530

    point system Page done

--- Server/index.js
+++ Server/index.js
@@ -2,63 +2,85 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(cors());
 
+const db = mysql.createConnection({
+  host: "ezpark-db.cvhbqqtsx1je.ap-northeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "ezPark!123",
+  database: "EzPark",
+});
 
-
-const db=mysql.createConnection({
-    host:"ezpark-db.cvhbqqtsx1je.ap-northeast-1.rds.amazonaws.com",
-    user:"admin",
-    password:"ezPark!123",
-    database:"EzPark"
-
-})
-
-app.get("/",(req,res)=>{
-    res.json("Hello this is backend of EzPark")
-})
+app.get("/", (req, res) => {
+  res.json("Hello this is backend of EzPark");
+});
 
 app.get("/badges", (req, res) => {
-    const q = "SELECT * FROM EzPark.Badge_Details;";
+  const q = "SELECT * FROM EzPark.Badge_Details;";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("/pointActions", (req, res) => {
+    const q = "SELECT * FROM EzPark.Point_Details;";
     db.query(q, (err, data) => {
       if (err) return res.json(err);
       return res.json(data);
     });
   });
 
-app.get("/user",(req,res)=>{
-    const query="SELECT * FROM User_Details"
-    db.query(query,(err,data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
+  app.get("/discounts", (req, res) => {
+    const q = "SELECT * FROM EzPark.Discounts_Details;";
+    db.query(q, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+  });
 
-app.post("/user",(req,res)=>{
-    const query="INSERT INTO `EzPark`.`User_Details` (`FirstName`, `LastName`, `AddFLine`, `AddSLine`, `Street`, `City`, `PostCode`, `MobileNo`, `FixedLine`, `NIC`, `Email`,`Password`) VALUES (?);"
-    const values=[
-        req.body.Fname,
-        req.body.Lname,
-        req.body.AddFLine,
-        req.body.AddSLine,
-        req.body.Street,
-        req.body.City,
-        req.body.PCode,
-        req.body.MobNum,
-        req.body.FixedNum,
-        req.body.Nic,
-        req.body.Email,
-        req.body.Pword
-    ]
-    db.query(query,[values],(err,data)=>{
-        if(err) return res.json(err)
-        return res.json("User account has been  created succefully")
-    })
+  app.get("/Refund_Level", (req, res) => {
+    const q = "SELECT * FROM EzPark.Refund_Level;";
+    db.query(q, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+  });
+  
 
-})
+app.get("/user", (req, res) => {
+  const query = "SELECT * FROM User_Details";
+  db.query(query, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-app.listen(8800, ()=>{
-    console.log("Connected to backend!!")
-})
+app.post("/user", (req, res) => {
+  const query =
+    "INSERT INTO `EzPark`.`User_Details` (`FirstName`, `LastName`, `AddFLine`, `AddSLine`, `Street`, `City`, `PostCode`, `MobileNo`, `FixedLine`, `NIC`, `Email`,`Password`) VALUES (?);";
+  const values = [
+    req.body.Fname,
+    req.body.Lname,
+    req.body.AddFLine,
+    req.body.AddSLine,
+    req.body.Street,
+    req.body.City,
+    req.body.PCode,
+    req.body.MobNum,
+    req.body.FixedNum,
+    req.body.Nic,
+    req.body.Email,
+    req.body.Pword,
+  ];
+  db.query(query, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("User account has been  created succefully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend!!");
+});
