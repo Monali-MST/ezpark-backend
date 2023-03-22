adbd55a530a794217108297dd8cb70d75c7b6198
Nathali Fernando <nathalifernando70@gmail.com>
2023-03-22 11:27:49 +0530

    Added static view parts to backend

--- Server/index.js
+++ Server/index.js
@@ -99,6 +99,8 @@ app.get("/pointActions", (req, res) => {
       return res.json(data);
     });
   });
+
+
 //------PointSystem user points increment functions Backend----------
 // app.get("/pointsadd", (req, res) => {
 //   const q = "SELECT * FROM incrememtdb.user_details;";
@@ -125,6 +127,182 @@ app.get("/pointActions", (req, res) => {
 //     return res.json("book has been updated successfully");
 //   });
 // });
+
+
+
+//------Static View Page Backend---------------------------------
+//BOOKINGS AND CANCELLATIONS STARTS
+app.get ("/bookingDaily",(req, res)=>{//daily dookings and cancellations
+  const query1="SELECT *FROM Booking WHERE BookedDate=date (now()) ;";
+  const query2= "SELECT * FROM BookingCancellation WHERE CancelDate=date (now()) ;";
+  let Booking, Cancellation;
+  db.query(query1, (error1, results1) => {
+      if (error1) return res.json(error1);
+      Booking = 0;
+      results1.forEach(row1 => {
+        Booking++;
+      });
+  
+      db.query(query2, (error2, results2) => {
+        if (error2) return res.json(error2);
+         Cancellation = 0;
+        results2.forEach(row2 => {
+          Cancellation ++;
+        });
+        res.json({ Booking,Cancellation });
+      });
+    });
+});
+
+app.get ("/bookingWeekly",(req, res)=>{//bookings and cancellation of the week
+  const query1="SELECT * FROM Booking where BookedDate between adddate(now(),-7) and now();"
+  const query2="SELECT * FROM BookingCancellation where CancelDate between adddate(now(),-7) and now();"
+  let Booking,Cancellation;
+  
+  db.query(query1, (error1, results1) => {
+    if (error1) return res.json(error1);
+    Booking = 0;
+    results1.forEach(row1 => {
+    Booking++;
+  });
+
+  db.query(query2, (error2, results2) => {
+    if (error2) return res.json(error2);
+    Cancellation = 0;
+    results2.forEach(row2 => {
+    Cancellation ++;
+  });
+
+    res.json({ Booking,Cancellation });
+    });
+  });
+});
+
+  app.get("/bookingMonthly", (req, res) => {//booking and cancellation of this month
+    const query1 = "SELECT * FROM Booking WHERE MONTH(BookedDate)=MONTH(now());";
+    const query2 = "SELECT * FROM BookingCancellation Where month(CancelDate)=month(now());";
+    let Booking,Cancellation;
+  
+    db.query(query1, (error1, results1, fields1) => {
+      if (error1) return res.json(error1);
+      Booking = 0;
+      results1.forEach(row1 => {
+        Booking++;
+      });
+  
+      db.query(query2, (error2, results2, fields2) => {
+        if (error2) return res.json(error2);
+         Cancellation = 0;
+        results2.forEach(row2 => {
+          Cancellation ++;
+        });
+
+        res.json({ Booking,Cancellation });
+      });
+    });
+  });
+//BOOKINGS AND CANCELLATIONS ENDS
+
+//REVENUE PART BEGINS
+app.get("/revenueMonthly", (req,res)=>{//MONTHLY
+  const queryMR="SELECT PaymentDate,SUM(PaymentAmount) AS TotalRevenueMonthly FROM Payment_Details WHERE MONTH(PaymentDate)=MONTH(now()) GROUP BY date(PaymentDate) ORDER BY date(PaymentDate);";
+  db.query(queryMR, (error, results, fields) =>{
+      if(error) return res.json(error)
+      const rows=results.map(row => {
+          const UserData={};
+          fields.forEach(field =>{
+              UserData[field.name] = row[field.name];
+          });
+          return UserData;
+      });
+      return  res.json(rows);
+      });
+})
+
+app.get("/revenueWeekly", (req,res)=>{//WEEKLY
+  const queryW="SELECT PaymentDate, (PaymentAmount) AS TotalRevenueWeekly FROM Payment_Details WHERE PaymentDate BETWEEN date (now())-6 AND date (now())+1 GROUP BY date(PaymentDate) ORDER BY date(PaymentDate);";
+  db.query(queryW, (error, results, fields) =>{
+      if(error) return res.json(error)
+      const rows=results.map(row => {
+          const UserData={};
+          fields.forEach(field =>{
+              console.log(row);
+              UserData[field.name] = row[field.name];
+          });
+          return UserData; 
+      });
+      return  res.json(rows);
+      });
+})
+
+app.get("/revenueDaily", (req,res)=>{//DAILY
+  const queryD="SELECT PaymentDate, SUM(PaymentAmount) AS TotalRevenueDaily FROM Payment_Details WHERE PaymentDate=date (now());";
+  db.query(queryD, (error, results, fields) =>{
+      if(error) return res.json(error)
+      const rows=results.map(row => {
+          const UserData={};
+          fields.forEach(field =>{
+              UserData[field.name] = row[field.name];
+          });
+          return UserData;
+      });
+      
+      return  res.json(rows); 
+      });
+})
+//REVENUE PART ENDS
+
+//FULL AND PARTIAL REFUNDS BEGIN
+app.get("/refundFPDaily", (req,res)=>{//DAILY
+  const queryRFPD="SELECT rd.RefundDate,COALESCE(full.TotalFullRefunds, 0) AS TotalFullRefunds, COALESCE(partial.TotalPartialRefunds, 0) AS TotalPartialRefunds FROM Refund_Details rd LEFT JOIN (SELECT date(RefundDate) AS RefundDate, SUM(Refund_amount) AS TotalFullRefunds FROM Refund_Details INNER JOIN Refund_Level ON Refund_Details.Refund_level_id = Refund_Level.Refund_level_id WHERE RefundDate=date(now()) AND Refund_percentage = 100 GROUP BY date(RefundDate)) full ON rd.RefundDate = full.RefundDate LEFT JOIN (SELECT date(RefundDate) AS RefundDate,SUM(Refund_amount) AS TotalPartialRefunds FROM Refund_Details INNER JOIN Refund_Level ON Refund_Details.Refund_level_id = Refund_Level.Refund_level_id WHERE RefundDate=date(now()) AND Refund_percentage = 50 GROUP BY date(RefundDate)) partial ON rd.RefundDate = partial.RefundDate WHERE rd.RefundDate = date(NOW()) GROUP BY rd.RefundDate;";
+  db.query(queryRFPD, (error, results, fields) =>{
+      if(error) return res.json(error)
+      const rows=results.map(row => {
+          const UserData={};
+          fields.forEach(field =>{
+              UserData[field.name] = row[field.name];
+          });
+          return UserData;
+      });
+      
+      return  res.json(rows); 
+      });
+})
+
+
+   app.get("/refundFPWeekly", (req,res)=>{//WEEKLY
+      const queryRFPW="SELECT rd.RefundDate,COALESCE(full.TotalFullRefunds, 0) AS TotalFullRefunds, COALESCE(partial.TotalPartialRefunds, 0) AS TotalPartialRefunds FROM Refund_Details rd LEFT JOIN (SELECT date(RefundDate) AS RefundDate, SUM(Refund_amount) AS TotalFullRefunds FROM Refund_Details INNER JOIN Refund_Level ON Refund_Details.Refund_level_id = Refund_Level.Refund_level_id WHERE RefundDate BETWEEN date (now())-6 AND date (now()) AND Refund_percentage = 100 GROUP BY date(RefundDate)) full ON rd.RefundDate = full.RefundDate LEFT JOIN (SELECT date(RefundDate) AS RefundDate,SUM(Refund_amount) AS TotalPartialRefunds FROM Refund_Details INNER JOIN Refund_Level ON Refund_Details.Refund_level_id = Refund_Level.Refund_level_id WHERE RefundDate BETWEEN date (now())-6 AND date (now()) AND Refund_percentage = 50 GROUP BY date(RefundDate)) partial ON rd.RefundDate = partial.RefundDate WHERE rd.RefundDate BETWEEN date (now())-6 AND date (now()) GROUP BY rd.RefundDate ORDER BY rd.RefundDate;";
+      db.query(queryRFPW, (error, results, fields) =>{
+          if(error) return res.json(error)
+          const rows=results.map(row => {
+              const UserData={};
+              fields.forEach(field =>{
+                  UserData[field.name] = row[field.name];
+              });
+              return UserData;
+          });
+          
+          return  res.json(rows); 
+          });
+  })   
+
+app.get("/refundFPMonthly", (req,res)=>{//MONTHLY
+  const queryRFPM="SELECT rd.RefundDate,COALESCE(full.TotalFullRefunds, 0) AS TotalFullRefunds, COALESCE(partial.TotalPartialRefunds, 0) AS TotalPartialRefunds FROM Refund_Details rd LEFT JOIN (SELECT date(RefundDate) AS RefundDate, SUM(Refund_amount) AS TotalFullRefunds FROM Refund_Details INNER JOIN Refund_Level ON Refund_Details.Refund_level_id = Refund_Level.Refund_level_id WHERE MONTH(RefundDate) = MONTH(NOW()) AND Refund_percentage = 100 GROUP BY date(RefundDate)) full ON rd.RefundDate = full.RefundDate LEFT JOIN (SELECT date(RefundDate) AS RefundDate,SUM(Refund_amount) AS TotalPartialRefunds FROM Refund_Details INNER JOIN Refund_Level ON Refund_Details.Refund_level_id = Refund_Level.Refund_level_id WHERE MONTH(RefundDate) = MONTH(NOW()) AND Refund_percentage = 50 GROUP BY date(RefundDate)) partial ON rd.RefundDate = partial.RefundDate WHERE MONTH(rd.RefundDate) = MONTH(NOW()) GROUP BY rd.RefundDate ORDER BY rd.RefundDate;";
+  db.query(queryRFPM, (error, results, fields) =>{
+      if(error) return res.json(error)
+      const rows=results.map(row => {
+          const UserData={};
+          fields.forEach(field =>{
+              UserData[field.name] = row[field.name];
+          });
+          return UserData;
+      });
+      
+      return  res.json(rows); 
+      });
+})
+//FULL AND PARTIAL REFUNDS END
+
 
 
 
